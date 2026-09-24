b0a295be0260569fca1513d4a161da43a4942cf4
Aaditya Gaur <aaditya2605@gmail.com>
2026-09-24 00:18:22 -0400

    List Paste and go on the About page.
    (cherry picked from commit a5785cc02ecb11bda7f4378dfb2bb39326838906)

--- Sources/Search/Settings.swift
+++ Sources/Search/Settings.swift
@@ -416,6 +416,8 @@ struct SettingsPanel: View {
                 Rule()
                 Shortcut("⌘T  ⌘W  ⇧⌘T", "New, close, reopen tab")
                 Rule()
+                Shortcut("⇧⌘V", "Paste and go")
+                Rule()
                 Shortcut("⌃⇥  ⌘1–9", "Next tab, a tab by its place")
                 Rule()
                 Shortcut("⇧⌘S", "Tabs in a sidebar")
